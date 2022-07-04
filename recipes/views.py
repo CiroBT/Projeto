6f8b14dae0ca988ffb2d5541709ac5510ebf60f4
Ciro Bezerra Torres <cbtorres89@gmail.com>
2022-07-03 22:18:32 -0300

    dynamic url & links, inheritance templates, blocks

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request=request, template_name="recipes/pages/home.html", context={"exemplo": "exemplo"})
+    return render(
+        request=request,
+        template_name="recipes/pages/home.html",
+        context={
+            "recipes": [make_recipe() for _ in range(10)],
+        }
+    )
+
+
+def recipe(request, id):
+    return render(
+        request=request,
+        template_name="recipes/pages/recipe-view.html",
+        context={
+            "recipe": make_recipe(),
+            "is_detail_page": True,
+        }
+    )
